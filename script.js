a44b8a3a5e4a7e05be2a121a84eb111e88854dd7
meicib <ameicib@berkeley.edu>
2024-11-17 13:58:30 -0800

    Styling

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ if (!code) {
 
         console.log(timeRange);
 
-        const limit = "10";
+        const limit = "15";
 
         const topTracks = await fetchWebApi(`v1/me/top/tracks?time_range=${timeRange}&limit=${limit}`, "GET", accessToken);
         displayTracks(topTracks);
@@ -87,18 +87,18 @@ export async function getAccessToken(clientId, code) {
 
 function populateUI(profile) {
     document.getElementById("displayName").innerText = profile.display_name;
-    if (profile.images[0]) {
-        const profileImage = new Image(200, 200);
-        profileImage.src = profile.images[0].url;
-        document.getElementById("avatar").appendChild(profileImage);
-        document.getElementById("imgUrl").innerText = profile.images[0].url;
-    }
-    document.getElementById("id").innerText = profile.id;
-    document.getElementById("email").innerText = profile.email;
-    document.getElementById("uri").innerText = profile.uri;
-    document.getElementById("uri").setAttribute("href", profile.external_urls.spotify);
-    document.getElementById("url").innerText = profile.href;
-    document.getElementById("url").setAttribute("href", profile.href);
+    // if (profile.images[0]) {
+    //     const profileImage = new Image(200, 200);
+    //     profileImage.src = profile.images[0].url;
+    //     document.getElementById("avatar").appendChild(profileImage);
+    //     document.getElementById("imgUrl").innerText = profile.images[0].url;
+    // }
+    // document.getElementById("id").innerText = profile.id;
+    // document.getElementById("email").innerText = profile.email;
+    // document.getElementById("uri").innerText = profile.uri;
+    // document.getElementById("uri").setAttribute("href", profile.external_urls.spotify);
+    // document.getElementById("url").innerText = profile.href;
+    // document.getElementById("url").setAttribute("href", profile.href);
 }
 
 async function fetchWebApi(endpoint, method, token, body) {
